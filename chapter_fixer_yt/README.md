2089b176deca9693092d991b2b23f2b9c09e657b
Jon Ingoglia <jon.ingoglia@gmail.com>
2023-09-06 19:52:59 -0400

    youtube chapter fixer

---
+++ chapter_fixer_yt/README.md
@@ -0,0 +1,7 @@
+# Instructions
+
+- Find a video on YouTube with timestamps in the comments (such as a concert video with a setlist in the comments)
+- Copy and paste that comment into input.txt and manually remove anything irregular. It should be just timestamps and chapter names (no "EN" for encore, for example).
+- Last line must be the length of the entire video followed by [END]
+- Run this script, it will output an XML file
+- Use MKVToolNix GUI to add the XML file to the video
---
+++ chapter_fixer_yt/chapter_fixer_yt.py
@@ -0,0 +1,47 @@
+# Generatea chapter files from timestamps in YouTube comments
+# by Jon Ingoglia
+
+import re
+
+metadata_file = "input.txt"
+
+try:
+    with open(metadata_file, "r", encoding="utf-8") as f:
+        metadata = f.read()
+except FileNotFoundError:
+    print(f"Error: {metadata_file} not found in the working directory.")
+    exit(1)
+
+lines = metadata.strip().split("\n")
+chapters = []
+
+for i in range(len(lines)):
+  if (re.split('\s', lines[i], 1)[1] != '[END]'):
+    start = re.split('\s', lines[i], 1)[0]
+    end = re.split('\s', lines[i + 1], 1)[0]
+    title = re.split('\s', lines[i], 1)[1].strip()
+    chapters.append((start, end, title))
+
+xml_content = '<?xml version="1.0" encoding="UTF-8"?>\n'
+xml_content += '<Chapters>\n'
+xml_content += '    <EditionEntry>\n'
+
+for i, (start, end, title) in enumerate(chapters):
+    print(f"{title}: {start}-{end}")
+    xml_content += f'        <ChapterAtom>\n'
+    xml_content += f'            <ChapterTimeStart>{start}</ChapterTimeStart>\n'
+    xml_content += f'            <ChapterTimeEnd>{end}</ChapterTimeEnd>\n'
+    xml_content += f'            <ChapterDisplay>\n'
+    xml_content += f'                <ChapterString>{title}</ChapterString>\n'
+    xml_content += f'                <ChapterLanguage>eng</ChapterLanguage>\n'
+    xml_content += f'            </ChapterDisplay>\n'
+    xml_content += f'        </ChapterAtom>\n'
+
+xml_content += '    </EditionEntry>\n'
+xml_content += '</Chapters>\n'
+
+output_file = 'output.xml'
+with open(output_file, 'w', encoding="utf-8") as xml_file:
+    xml_file.write(xml_content)
+
+print(f"XML content has been written to {output_file}")
